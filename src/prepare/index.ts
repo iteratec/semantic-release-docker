12689c1901a465136d1fa6c0cb83c3d6d30c119e
Daniel Habenicht <daniel-habenicht@outlook.de>
2018-11-23 10:11:43 +0100

    fix(plugin): Additional tags are pushed to repo now
    additional tags were not persisted to the tags array as concat() does not change any of the arrays.
    
    #2

--- src/prepare/index.ts
+++ src/prepare/index.ts
@@ -13,25 +13,25 @@ export async function prepare(pluginConfig: SemanticReleaseConfig, context: Sema
   }
   const docker = new Dockerode();
   const image = docker.getImage(preparePlugin.imageName);
-  const tags = [context.nextRelease!.version!];
+  let tags = [context.nextRelease!.version!];
   if (preparePlugin.additionalTags && preparePlugin.additionalTags.length > 0) {
-    tags.concat(preparePlugin.additionalTags);
+    tags = tags.concat(preparePlugin.additionalTags);
   }
   return Promise.all(tags.map((imagetag) => {
     return image.tag({
       repo: `${preparePlugin.registryUrl ? `${preparePlugin.registryUrl}/` : ''}` +
-      `${preparePlugin.repositoryName ? `${preparePlugin.repositoryName}/` : ''}` +
-      `${preparePlugin.imageName}`,
+        `${preparePlugin.repositoryName ? `${preparePlugin.repositoryName}/` : ''}` +
+        `${preparePlugin.imageName}`,
       tag: imagetag,
     });
   }))
-  .then((data) => {
-    if (!prepared) {
-      prepared = true;
-    }
-    return data.map((result) => result.name);
-  })
-  .catch((error) => {
-    throw new Error(error);
-  });
+    .then((data) => {
+      if (!prepared) {
+        prepared = true;
+      }
+      return data.map((result) => result.name);
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
 }
