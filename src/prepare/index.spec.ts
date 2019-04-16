50e77818f8fbd237788274b371b73f8ef0b8cb03
Daniel Habenicht <daniel-habenicht@outlook.de>
2019-04-16 23:45:13 +0200

    Update Prepare Tests

--- src/prepare/index.spec.ts
+++ src/prepare/index.spec.ts
@@ -5,6 +5,7 @@ import Dockerode from 'dockerode';
 import { SemanticReleaseConfig, SemanticReleaseContext } from 'semantic-release';
 import { DockerPluginConfig } from '../models';
 import { prepare } from './index';
+import { setVerified } from '../verifyConditions';
 
 describe('@iteratec/semantic-release-docker', function() {
   describe('prepare', function() {
@@ -12,150 +13,136 @@ describe('@iteratec/semantic-release-docker', function() {
       branch: '',
       noCi: true,
       repositoryUrl: '',
-      tagFormat: '',
+      tagFormat: ''
     };
+
+    const testImage1 = 'test1:latest';
+    const testImage2 = 'test2:latest';
 
     before(function() {
       use(chaiAsPromised);
+      setVerified();
     });
 
     before(async function() {
       this.timeout(10000);
       const docker = new Dockerode();
-      docker.buildImage(
+      await docker.buildImage(
         {
           context: './',
-          src: ['Dockerfile'],
+          src: ['Dockerfile']
         },
         {
-          t: 'test1:latest',
+          t: testImage1
         },
         function(error, output) {
           if (error) {
             return console.error(error);
           }
-        },
+        }
       );
-      docker.buildImage(
+      await docker.buildImage(
         {
           context: './',
-          src: ['Dockerfile'],
+          src: ['Dockerfile']
         },
         {
-          t: 'test2:latest',
+          t: testImage2
         },
         function(error, output) {
           if (error) {
             return console.error(error);
           }
-        },
+        }
       );
       process.env.DOCKER_REGISTRY_USER = 'username';
       process.env.DOCKER_REGISTRY_PASSWORD = 'password';
     });
 
-    it('should throw if no imagename is provided', function() {
+    it('should tag an image', function() {
       const context = {
         // tslint:disable-next-line:no-empty
         logger: { log: (message: string) => {} },
         nextRelease: {
           gitTag: '',
           notes: '',
-          version: 'next',
+          version: 'next'
         },
         options: {
           branch: '',
           noCi: true,
           prepare: [
             {
-              path: '@iteratec/semantic-release-docker',
-            } as DockerPluginConfig,
+              imageName: testImage1,
+              path: '@iteratec/semantic-release-docker'
+            } as DockerPluginConfig
           ],
           repositoryUrl: '',
-          tagFormat: '',
-        },
+          tagFormat: ''
+        }
       } as SemanticReleaseContext;
-      return expect(prepare(config, context)).to.be.rejectedWith('\'imageName\' is not set in plugin configuration');
+      return expect(prepare(config, context)).to.eventually.deep.equal([[testImage1]]);
     });
 
-    //   it('should tag an image', function() {
-    //     const context = {
-    //       // tslint:disable-next-line:no-empty
-    //       logger: { log: (message: string) => {} },
-    //       nextRelease: {
-    //         gitTag: '',
-    //         notes: '',
-    //         version: 'next',
-    //       },
-    //       options: {
-    //         branch: '',
-    //         noCi: true,
-    //         prepare: [
-    //           {
-    //             imageName: 'test1',
-    //             path: '@iteratec/semantic-release-docker',
-    //           } as DockerPluginConfig,
-    //         ],
-    //         repositoryUrl: '',
-    //         tagFormat: '',
-    //       },
-    //     } as SemanticReleaseContext;
-    //     return expect(prepare(config, context)).to.eventually.deep.equal([['test1']]);
-    //   });
+    it('should add multiple tags to an image', function() {
+      const context = {
+        // tslint:disable-next-line:no-empty
+        logger: { log: (message: string) => {} },
+        nextRelease: {
+          gitTag: '',
+          notes: '',
+          version: 'next'
+        },
+        options: {
+          branch: '',
+          noCi: true,
+          prepare: [
+            {
+              imageName: testImage1,
+              path: '@iteratec/semantic-release-docker',
+              additionalTags: ['tag1', 'tag2']
+            } as DockerPluginConfig
+          ],
+          repositoryUrl: '',
+          tagFormat: ''
+        }
+      } as SemanticReleaseContext;
+      return expect(prepare(config, context).then(data => data[0])).to.eventually.have.length(3);
+    });
 
-    //   it('should add multiple tags to an image', function() {
-    //     const context = {
-    //       // tslint:disable-next-line:no-empty
-    //       logger: { log: (message: string) => {} },
-    //       nextRelease: {
-    //         gitTag: '',
-    //         notes: '',
-    //         version: 'next',
-    //       },
-    //       options: {
-    //         branch: '',
-    //         noCi: true,
-    //         prepare: [
-    //           {
-    //             imageName: 'test1',
-    //             path: '@iteratec/semantic-release-docker',
-    //             additionalTags: ['tag1', 'tag2'],
-    //           } as DockerPluginConfig,
-    //         ],
-    //         repositoryUrl: '',
-    //         tagFormat: '',
-    //       },
-    //     } as SemanticReleaseContext;
-    //     return expect(prepare(config, context).then((data) => data[0])).to.eventually.have.length(3);
-    //   });
+    it('should add multiple images', function() {
+      const context = {
+        // tslint:disable-next-line:no-empty
+        logger: { log: (message: string) => {} },
+        nextRelease: {
+          gitTag: '',
+          notes: '',
+          version: 'next'
+        },
+        options: {
+          branch: '',
+          noCi: true,
+          prepare: [
+            {
+              imageName: testImage1,
+              path: '@iteratec/semantic-release-docker'
+            } as DockerPluginConfig,
+            {
+              imageName: 'test2',
+              path: '@iteratec/semantic-release-docker'
+            } as DockerPluginConfig
+          ],
+          repositoryUrl: '',
+          tagFormat: ''
+        }
+      } as SemanticReleaseContext;
+      return expect(prepare(config, context)).to.eventually.have.length(2);
+    });
 
-    //   it('should add multiple images', function() {
-    //     const context = {
-    //       // tslint:disable-next-line:no-empty
-    //       logger: { log: (message: string) => {} },
-    //       nextRelease: {
-    //         gitTag: '',
-    //         notes: '',
-    //         version: 'next',
-    //       },
-    //       options: {
-    //         branch: '',
-    //         noCi: true,
-    //         prepare: [
-    //           {
-    //             imageName: 'test1',
-    //             path: '@iteratec/semantic-release-docker',
-    //           } as DockerPluginConfig,
-    //           {
-    //             imageName: 'test2',
-    //             path: '@iteratec/semantic-release-docker',
-    //           } as DockerPluginConfig,
-    //         ],
-    //         repositoryUrl: '',
-    //         tagFormat: '',
-    //       },
-    //     } as SemanticReleaseContext;
-    //     return expect(prepare(config, context)).to.eventually.have.length(2);
-    //   });
+    after(async function() {
+      const docker = new Dockerode();
+      docker.getImage(testImage1).remove();
+      docker.getImage(testImage2).remove();
+    });
   });
 });
