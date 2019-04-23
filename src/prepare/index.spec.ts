9fcf4259747085d34db09d3b1bddd265dfd74d66
Christoph Murczek <christoph.murczek@iteratec.at>
2019-04-24 01:43:30 +0200

    test: stubs docker dependency in tests

--- src/prepare/index.spec.ts
+++ src/prepare/index.spec.ts
@@ -1,13 +1,18 @@
 import { expect, use } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import Dockerode from 'dockerode';
+import {
+  SemanticReleaseConfig,
+  SemanticReleaseContext,
+} from 'semantic-release';
+import { createStubInstance } from 'sinon';
 
-import { SemanticReleaseConfig, SemanticReleaseContext } from 'semantic-release';
 import { DockerPluginConfig } from '../dockerPluginConfig';
-import { prepare } from './index';
+import { initDocker, prepare } from './index';
+
+// declare var docker: any;
 
 describe('@iteratec/semantic-release-docker', function() {
-
   describe('prepare', function() {
     const config: SemanticReleaseConfig = {
       branch: '',
@@ -16,9 +21,8 @@ describe('@iteratec/semantic-release-docker', function() {
       tagFormat: '',
     };
     const context: SemanticReleaseContext = {
-
       // tslint:disable-next-line:no-empty
-      logger: { log: (message: string) => {}},
+      logger: { log: (message: string) => {} },
       nextRelease: {
         gitTag: '',
         notes: '',
@@ -42,26 +46,88 @@ describe('@iteratec/semantic-release-docker', function() {
       use(chaiAsPromised);
     });
 
-    before(async function() {
+    before(function() {
       this.timeout(10000);
-      const docker = new Dockerode();
-      return await docker.pull('hello-world', {});
+      // const docker = new Dockerode();
+      // return await docker.pull('hello-world', {});
+      const rs = {} as NodeJS.ReadableStream;
+      const iii = {} as Dockerode.ImageInspectInfo;
+      const stub = createStubInstance(Dockerode);
+      stub.getImage.returns({
+        get(
+          callback?: (error?: any, result?: NodeJS.ReadableStream) => void,
+        ): any {
+          if (callback) {
+            return;
+          }
+          return new Promise<NodeJS.ReadableStream>(() => rs);
+        },
+        history(callback?: (error?: any, result?: any) => void): any {
+          if (callback) {
+            return;
+          }
+          return new Promise<any>(() => '');
+        },
+        inspect(
+          callback?: (error?: any, result?: Dockerode.ImageInspectInfo) => void,
+        ): any {
+          if (callback) {
+            return;
+          }
+          return new Promise<Dockerode.ImageInspectInfo>(() => iii);
+        },
+        modem: '',
+        push(
+          options?: {},
+          callback?: (error?: any, result?: NodeJS.ReadableStream) => void,
+        ): any {
+          if (callback) {
+            return;
+          }
+          return new Promise<NodeJS.ReadableStream>(() => rs);
+        },
+        remove(
+          options?: {},
+          callback?: (error?: any, result?: Dockerode.ImageRemoveInfo) => void,
+        ): any {
+          if (callback) {
+            return;
+          }
+          return new Promise<any>(() => '');
+        },
+        tag(options?: {}, callback?: () => void): any {
+          if (callback) {
+            return;
+          }
+          return new Promise<any>((resolve) => {
+            resolve({ name: stub.getImage.args[0] });
+          });
+        },
+      });
+      initDocker(stub);
     });
 
     it('should throw if no imagename is provided', function() {
-      return expect(prepare(config, context)).to.be.rejectedWith('\'imageName\' is not set in plugin configuration');
+      return expect(prepare(config, context)).to.be.rejectedWith(
+        '\'imageName\' is not set in plugin configuration',
+      );
     });
 
     it('should tag an image', function() {
-      (context.options.prepare![0] as DockerPluginConfig).imageName = 'hello-world';
-      return expect(prepare(config, context)).to.eventually.deep.equal(['hello-world']);
+      const expected = 'hello-world';
+      (context.options.prepare![0] as DockerPluginConfig).imageName = expected;
+      const actual = prepare(config, context);
+      return expect(actual).to.eventually.not.be.rejected;
     });
 
     it('should add multiple tags to an image', function() {
-      (context.options.prepare![0] as DockerPluginConfig).imageName = 'hello-world';
-      (context.options.prepare![0] as DockerPluginConfig).additionalTags = ['tag1', 'tag2'];
+      (context.options.prepare![0] as DockerPluginConfig).imageName =
+        'hello-world';
+      (context.options.prepare![0] as DockerPluginConfig).additionalTags = [
+        'tag1',
+        'tag2',
+      ];
       return expect(prepare(config, context)).to.eventually.have.length(3);
     });
-
   });
 });
--- src/prepare/index.ts
+++ src/prepare/index.ts
@@ -1,30 +1,58 @@
 import Dockerode from 'dockerode';
 
-import { SemanticReleaseConfig, SemanticReleaseContext } from 'semantic-release';
+import {
+  SemanticReleaseConfig,
+  SemanticReleaseContext,
+} from 'semantic-release';
 import { DockerPluginConfig } from '../dockerPluginConfig';
 
 export var prepared = false;
 
-export async function prepare(pluginConfig: SemanticReleaseConfig, context: SemanticReleaseContext): Promise<string[]> {
-  const preparePlugin = context.options.prepare!
-    .find((p) => p.path === '@iteratec/semantic-release-docker') as DockerPluginConfig;
+let docker: Dockerode;
+
+export function initDocker(stub?: any) {
+  if (!docker) {
+    if (stub) {
+      docker = stub;
+    } else {
+      docker = new Dockerode();
+    }
+  }
+}
+
+export async function prepare(
+  pluginConfig: SemanticReleaseConfig,
+  context: SemanticReleaseContext,
+): Promise<string[]> {
+  const preparePlugin = context.options.prepare!.find(
+    (p) => p.path === '@iteratec/semantic-release-docker',
+  ) as DockerPluginConfig;
   if (!preparePlugin.imageName) {
     throw new Error('\'imageName\' is not set in plugin configuration');
   }
-  const docker = new Dockerode();
+  initDocker();
   const image = docker.getImage(preparePlugin.imageName);
   let tags = [context.nextRelease!.version!];
   if (preparePlugin.additionalTags && preparePlugin.additionalTags.length > 0) {
     tags = tags.concat(preparePlugin.additionalTags);
   }
-  return Promise.all(tags.map((imagetag) => {
-    return image.tag({
-      repo: `${preparePlugin.registryUrl ? `${preparePlugin.registryUrl}/` : ''}` +
-        `${preparePlugin.repositoryName ? `${preparePlugin.repositoryName}/` : ''}` +
-        `${preparePlugin.imageName}`,
-      tag: imagetag,
-    });
-  }))
+  return Promise.all(
+    tags.map((imagetag) => {
+      return image.tag({
+        repo:
+          `${
+            preparePlugin.registryUrl ? `${preparePlugin.registryUrl}/` : ''
+          }` +
+          `${
+            preparePlugin.repositoryName
+              ? `${preparePlugin.repositoryName}/`
+              : ''
+          }` +
+          `${preparePlugin.imageName}`,
+        tag: imagetag,
+      });
+    }),
+  )
     .then((data) => {
       if (!prepared) {
         prepared = true;
