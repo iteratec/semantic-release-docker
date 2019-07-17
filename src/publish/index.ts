c0d941618d084674be7c488b37b4479f99e69e73
Daniel Habenicht <daniel-habenicht@outlook.de>
2019-07-17 11:42:43 +0200

    chore(publish): use getCredentials insted of env

--- src/publish/index.ts
+++ src/publish/index.ts
@@ -1,10 +1,9 @@
 import Dockerode from 'dockerode';
-
 import { SemanticReleaseConfig, SemanticReleaseContext } from 'semantic-release';
 import { Authentication, DockerPluginConfig } from '../models';
 import { pluginSettings } from '../plugin-settings';
 import { prepare, prepared } from '../prepare';
-import { constructImageName, getRegistryUrlFromConfig, getImageTagsFromConfig } from '../shared-logic';
+import { constructImageName, getCredentials, getImageTagsFromConfig, getRegistryUrlFromConfig } from '../shared-logic';
 
 interface PushOptions extends Authentication {
   tag: string;
@@ -18,62 +17,62 @@ export async function publish(pluginConfig: SemanticReleaseConfig, context: Sema
   if (!prepared) {
     await prepare(pluginConfig, context).then(
       () => {},
-      reject => {
+      (reject) => {
         return Promise.reject(reject);
-      }
+      },
     );
   }
 
   const docker = new Dockerode();
 
-  const preparePlugins = context.options.prepare!.filter(p => p.path === pluginSettings.path) as DockerPluginConfig[];
+  const preparePlugins = context.options.prepare!.filter((p) => p.path === pluginSettings.path) as DockerPluginConfig[];
 
   return Promise.all(
-    preparePlugins.map(preparePlugin => {
+    preparePlugins.map((preparePlugin) => {
       const tags = getImageTagsFromConfig(preparePlugin, context);
-
       const imageName = constructImageName(preparePlugin);
-
       const image = docker.getImage(imageName);
+      const cred = getCredentials();
+
       const options: PushOptions = {
-        password: process.env.DOCKER_REGISTRY_PASSWORD!,
+        password: cred.password,
         serveraddress: getRegistryUrlFromConfig(preparePlugin),
         tag: '',
-        username: process.env.DOCKER_REGISTRY_USER!
+        username: cred.username,
       };
       return Promise.all(
         tags.map((imageTag: string) => {
           options.tag = imageTag;
           context.logger.log(`pushing image ${imageName}:${imageTag}`);
           return image.push(options);
-        })
+        }),
       )
-        .then(streams =>
+        .then((streams) =>
           Promise.all(
             streams.map(
-              stream =>
+              (stream) =>
                 new Promise((resolve, reject) => {
-                  stream.on('data', chunk => context.logger.log(chunk.toString()));
+                  stream.on('data', (chunk) => context.logger.log(chunk.toString()));
                   stream.on('end', () => resolve());
-                  stream.on('error', error => {
+                  stream.on('error', (error) => {
                     reject(error);
                   });
-                })
-            )
-          )
+                }),
+            ),
+          ),
         )
         .then(() => {
           return {
-            completeImageName: tags.map((tag: string) => `${imageName}:${tag}`)
+            completeImageName: tags.map((tag: string) => `${imageName}:${tag}`),
           } as PublishedRelease;
         })
-        .catch(error => {
+        .catch((error) => {
           throw new Error(error);
         });
-    })
-  ).then(publishedImages => {
+    }),
+  ).then((publishedImages) => {
     return {
-      publishedImages: publishedImages
+      publishedImages,
     };
   });
 }
